5d57cd14452f08c1a27835833796683c56db3f4e
Subodh <subodhbhyri811@gmail.com>
2023-07-18 19:21:18 +0530

    initial commit

---
+++ middlewares/error.js
@@ -0,0 +1,21 @@
+class ErrorHandler extends Error{
+
+    constructor(message, statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+export const errorMiddleware = (err,req,res,next)=>{
+
+    err.message = err.message || "Internal Server Error";
+    err.statusCode = err.statusCode || 500;
+
+    return res.status(404).json({
+        success: false,
+        message: "Invalid id"
+    });
+
+}
+
+export default ErrorHandler;
